75947e30b7438b9e88cd55759b5ba02f11942b12
TengSear <tengsear4747gmail.com>
2024-07-12 20:18:20 +0700

    update

---
+++ src/components/Inventory.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="container mx-auto py-8">
+        <div class="flex flex-nowrap">
+            <div class="bg-white p-6 rounded-lg shadow-lg">
+                <h2 class="text-2xl font-bold mb-4">Overall Inventory</h2>
+                <div class="grid grid-cols-5 gap-4">
+                
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,9 +1,58 @@
 <template>
-    <div class="w-full p-4">
-        <div class="box bg-white shadow-lg rounded-lg overflow-hidden py-2 text-center">
-            <div class="box-header bg-gray-100 p-4 font-bold border-b">
-                Product List
+    <div class="container mx-auto py-8">
+
+        <div class="bg-white p-6 rounded-lg shadow-lg">
+            <h2 class="text-2xl font-bold mb-4">Overall Inventory</h2>
+            <div class="grid grid-cols-5 gap-4">
+                <div class="text-center">
+                    <h2 class="text-blue-700 flex ">Categories</h2>
+                    <p class="text-lg font-semibold flex">2</p>
+                    <p class="text-sm text-gray-600 flex">Last 7 days</p>
+                </div>
+                <div class="text-center bg-slate-600">
+                    <h2 class="text-orange-700 ">Total Product</h2>
+
+                    <p class="text-lg font-semibold text-left">868</p>
+                    <h class="text-sm text-gray-600 text-l">Electronic</h>
+                    <p class="text-lg font-semibold text-right">25000</p>
+                    <p class="text-sm text-gray-600 text-right">Furniture</p>
+
+                </div>
+                <div class="text-center bg-slate-300">
+                    <h2 class="text-cyan-500">In Person</h2>
+                    <div>
+                        <p class="text-lg font-semibold">5</p>
+                        <p class="text-lg font-semibold text-right">9</p>
+                        <p class="text-lg text-gray-600">Laptop</p>
+                        <p class="text-lg text-gray-600 text-right">IMac M1</p>
+                    </div>
+
+                </div>
+                <div class="text-center">
+                    <h2 class="text-red-700">In Stocks</h2>
+                    <p class="text-lg font-semibold">12</p>
+                    <p class="text-lg font-semibold">2</p>
+                    <p class="text-sm text-gray-600">Laptop</p>
+                    <p class="text-sm text-gray-600">IMac M1</p>
+                </div>
+
+
+
             </div>
+        </div>
+
+    </div>
+    <div class="container mx-auto py-8">
+
+        <div class="box bg-white shadow-lg rounded-lg overflow-hidden">
+
+            <h2 class="text-2xl font-bold mb-4"> Product</h2>
+            <button @click="addItem"
+                class="mb-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700  flex-nowarp">
+                Add Product
+            </button>
+
+
             <div class="card-body">
                 <table class="py-2 text-center w-full">
 
@@ -15,26 +64,30 @@
 
                             <th class="border border-slate-300 py-2 text-center">Purchase</th>
                             <th class="border border-slate-300 py-2 text-center">Quantity</th>
-                            <th class="border border-slate-300 py-2 text-center">User</th>
+                            <th class="border border-slate-300 py-2 text-center"> User</th>
                             <th class="border border-slate-300 py-2 text-center">Image</th>
-                            <th class="border border-slate-300 py-2 text-center">Location and Condition</th>
+                            <th class="border border-slate-300 py-2 text-center">Location</th>
                             <th class="border border-slate-300 py-2 text-center">Code</th>
                             <th class="border border-slate-300 py-2 text-center">Availability</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr v-for="(product, index) in products" :key="index" class="bg-white hover:bg-gray-100">
-                            <td class="border border-slate-300 py-2 text-center">{{ product.name }}</td>
-                            <td class="border border-slate-300 py-2 text-center">{{ product.Description }}</td>
-                            <td class="border border-slate-300 py-2 text-center">{{ product.purchase }}</td>
-                            <td class="border border-slate-300 py-2 text-center">{{ product.quantity }}</td>
-                            <td class="border border-slate-300 py-2 text-center">{{ product.user }}</td>
-                            <td class="border border-slate-300 py-2 text-center">
-                                <img :src="product.image" class="w-16 h-16 abject-cover" />
-                            </td>
-                            <td class="border border-slate-300 py-2 text-center">{{ product.LocationCondition }}</td>
-                            <td class="border border-slate-300 py-2 text-center">{{ product.code }}</td>
-                            <td class="border border-slate-300 py-2 text-center">{{ product.Availability }}</td>
+                        <tr>
+                            <th class="border border-slate-300 py-2 text-center font-light">IMac</th>
+                            <th class="border border-slate-300 py-2 text-center font-light">Automatic Hand Dryer/Product
+                                Code: EcoFast (SRA) /220617003</th>
+                            <th class="border border-slate-300 py-2 text-center font-light">08-02-2022</th>
+                            <th class="border border-slate-300 py-2 text-center font-light">1</th>
+                            <th class="border border-slate-300 py-2 text-center font-light">sophavin</th>
+
+                            <th class="border border-slate-300 py-2 text-center font-light"><img
+                                    src="../assets/iMac.png" class="max-w-xs h-16 rounded-lg shadow-lg">
+                            </th>
+                            <th class="border border-slate-300 py-2 text-center font-light">1st</th>
+                            <th class="border border-slate-300 py-2 text-center font-light ">HEIP-MT800008</th>
+                            <th class="border border-slate-300 py-2 text-center font-light text-emerald-400">In-Person
+                            </th>
+
                         </tr>
                     </tbody>
                 </table>
@@ -42,103 +95,8 @@
         </div>
     </div>
 </template>
+
 <script>
 
-export default {
-    name: 'ProductTable',
-    data() {
-        return {
-            products: [
-                {
-                    name: 'IMac M1',
-                    Description: 'Automatic Hand Dryer/Product Code EcoFast (SRA)/2206170003',
-                    purchase: '08-02-2022 ',
-                    quantity: '1',
-                    user: 'WC',
-                    image: 'https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/refurb-imac-27-2020?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1654865822599',
-                    LocationCondition: '1stFloor',
-                    code: 'HEIP-MT800008',
-                    Availability: 'In-Person'
-                },
-                {
-                    name: '',
-                    Description: ' ',
-                    purchase: '',
-                    quantity: '',
-                    user: '',
-                    image: '',
-                    LocationCondition: '',
-                    code: '',
-                    Availability: 'Out of stock'
-                },
-                {
-                    name: '',
-                    Description: ' ',
-                    purchase: '',
-                    quantity: '',
-                    user: '',
-                    image: '',
-                    LocationCondition: '',
-                    code: '',
-                    Availability: 'In- stock'
-                },
-                {
-                    name: '',
-                    Description: ' ',
-                    purchase: '',
-                    quantity: '',
-                    user: '',
-                    image: '',
-                    LocationCondition: '',
-                    code: '',
-                    Availability: 'Out of stock'
-                },
-                {
-                    name: '',
-                    Description: ' ',
-                    purchase: '',
-                    quantity: '',
-                    user: '',
-                    image: '',
-                    LocationCondition: '',
-                    code: '',
-                    Availability: 'In- stock'
-                },
-                {
-                    name: '',
-                    Description: ' ',
-                    purchase: '',
-                    quantity: '',
-                    user: '',
-                    image: '',
-                    LocationCondition: '',
-                    code: '',
-                    Availability: 'In- stock'
-                },
-                {
-                    name: '',
-                    Description: ' ',
-                    purchase: '',
-                    quantity: '',
-                    user: '',
-                    image: '',
-                    LocationCondition: '',
-                    code: '',
-                    Availability: 'Out of stock'
-                },
-                {
-                    name: '',
-                    Description: ' ',
-                    purchase: '',
-                    quantity: '',
-                    user: '',
-                    image: '',
-                    LocationCondition: '',
-                    code: '',
-                    Availability: 'In- stock'
-                }
-            ]
-        }
-    }
-};
+
 </script>
